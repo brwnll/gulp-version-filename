07f7226f5ccea1678a06f53c0b4e14bcd2292a95
dustin <dustin@smalldo.gs>
2015-08-10 05:07:53 -0700

    Adding sourcemap test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
 var test = require('tape');
 var	Vinyl = require('vinyl');
 var	filever = require('../');
+var uglifyjs = require('uglify-js');
+var sourcemaps = require('gulp-sourcemaps');
 
 test('should rename file without options', function(t) {
 	t.plan(7);
@@ -9,8 +11,8 @@ test('should rename file without options', function(t) {
 	var testFile1 = new Vinyl({
 		cwd: "/home/brwnll/broken-promises/",
 		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test1.js",
-		contents: new Buffer('/* @version 100.4.N */')
+		path: "/home/brwnll/broken-promises/test/test.js",
+		contents: new Buffer('/* @version 100.4.NEXT */')
 	});
 
 	var stream = filever();
@@ -24,7 +26,7 @@ test('should rename file without options', function(t) {
     t.ok(newFile instanceof Vinyl, 'file is Vinyl');
 		t.ok(newFile.contents instanceof Buffer, 'file contents are a buffer');
 
-    t.equals(newFile.relative, 'test1-100.4.N.js');
+    t.equals(newFile.relative, 'test-100.4.NEXT.js');
 	});
 
 	stream.write(testFile1);
@@ -37,15 +39,15 @@ test('should rename file with specified key', function(t) {
 	var testFile1 = new Vinyl({
 		cwd: "/home/brwnll/broken-promises/",
 		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test1.js",
-		contents: new Buffer('/* @specialKey 100.4.N */')
+		path: "/home/brwnll/broken-promises/test/test.js",
+		contents: new Buffer('/* @specialKey 100.4.NEXT */')
 	});
 
 	var stream = filever({ key: 'specialKey' });
 
 	stream.on('data', function(newFile) {
 
-    t.equals(newFile.relative, 'test1-100.4.N.js');
+    t.equals(newFile.relative, 'test-100.4.NEXT.js');
 	});
 
 	stream.write(testFile1);
@@ -80,13 +82,13 @@ test('should ignore error when silenced', function(t) {
 	var testFile1 = new Vinyl({
 		cwd: "/home/brwnll/broken-promises/",
 		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test1.js",
-		contents: new Buffer('/* @specialKey 100.4.N */')
+		path: "/home/brwnll/broken-promises/test/test.js",
+		contents: new Buffer('/* @specialKey 100.WRONGKEY */')
 	});
 
 	var stream = filever({ silent: true });
 
-	stream.on('data', function(newFile) {	});
+	stream.on('data', function(){ });
 
   stream.on('error', function(err) { t.end(err) })
 
@@ -95,3 +97,28 @@ test('should ignore error when silenced', function(t) {
 	stream.write(testFile1);
 	stream.end();
 });
+
+test('should update sourcemaps', function(t) {
+	t.plan(2);
+
+	var testContent = '/* @version 0.0.1 */(function(first, second) {\n console.log(first + second);\n}(5, 10));\n';
+
+	var testFile = new Vinyl({
+		cwd: "/home/brwnll/broken-promises/",
+		base: "/home/brwnll/broken-promises/test",
+		path: "/home/brwnll/broken-promises/test/test.js",
+		contents: new Buffer(testContent)
+	});
+
+	var sm = sourcemaps.init();
+
+	var renamePipe = sm.pipe(filever());
+
+	renamePipe.on('data', function(newFile) {
+		t.ok(newFile.sourceMap, 'has a source map');
+		t.equals(newFile.sourceMap.file, 'test-0.0.1.js', 'renamed file in sourcefile link')
+	});
+
+	sm.write(testFile);
+	sm.end();
+})
