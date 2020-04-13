30f74c8acd08b047bb53b8ad8ff2793e7ef0caaf
brwnll <dustin@smalldo.gs>
2020-04-13 11:00:09 -0700

    update deps

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-'use strict';
+const Stream = require("stream");
+const Path = require("path");
 
-var Stream = require('stream');
-var Path = require('path');
-
-module.exports = function(options) {
-
-  var stream = new Stream.Transform({ objectMode: true });
+module.exports = function (options) {
+  const stream = new Stream.Transform({ objectMode: true });
 
   options = options || {};
 
   stream._transform = function (file, filetype, callback) {
-    var contents = file.contents.toString();
-    var version;
-    var versionRegEx;
-    var components;
+    let contents = file.contents.toString();
+    let version;
+    let versionRegEx;
+    let components;
 
     // Load up option parameters
-    var key = options.key || 'version';
+    let key = options.key || "version";
 
     // indexOf is faster than running regex. As we are dealing with files
     // of an unknown size, check if the key exists first
-    if (contents.indexOf('@' + key) < 0) {
+    if (contents.indexOf("@" + key) < 0) {
       // If user wants to ignore files without the key, don't make a fuss
       if (options.silent) return callback(null, file);
       // Otherwise, raise hell
-      return stream.emit('error', new Error('Missing version key @' + key + ' in ' + file.relative));
+      return stream.emit(
+        "error",
+        new Error("Missing version key @" + key + " in " + file.relative)
+      );
     }
 
     // The first result is the entire match, the second is only the version string
-    versionRegEx = new RegExp('@' + key + ' ([^ ]*)', 'i');
+    versionRegEx = new RegExp("@" + key + " ([^ ]*)", "i");
     version = contents.match(versionRegEx)[1];
-    version = version.trim()
+    version = version.trim();
     // Break up the file name to splice in our version
-    components = file.relative.split('.');
+    components = file.relative.split(".");
 
     // File extension should be the final component, add the version to the
     // segment before it
-    components[components.length - 2] += '-' + version;
-    file.path = Path.join(file.base, components.join('.')).replace("\n", "");
-    
+    components[components.length - 2] += "-" + version;
+    file.path = Path.join(file.base, components.join(".")).replace("\n", "");
+
     // Rename sourcemap if present
     if (file.sourceMap) {
       file.sourceMap.file = file.relative;
@@ -48,4 +48,4 @@ module.exports = function(options) {
   };
 
   return stream;
-}
+};
--- test/test.js
+++ test/test.js
@@ -1,124 +1,130 @@
-'use strict';
-var test = require('tape');
-var	Vinyl = require('vinyl');
-var	filever = require('../');
-var uglifyjs = require('uglify-js');
-var sourcemaps = require('gulp-sourcemaps');
+const test = require("tape");
+const Vinyl = require("vinyl");
+const sourcemaps = require("gulp-sourcemaps");
+const filever = require("../");
 
-test('should rename file without options', function(t) {
-	t.plan(7);
+test("should rename file without options", function (t) {
+  t.plan(7);
 
-	var testFile1 = new Vinyl({
-		cwd: "/home/brwnll/broken-promises/",
-		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test.js",
-		contents: new Buffer('/* @version 100.4.NEXT */')
-	});
+  var testFile1 = new Vinyl({
+    cwd: "/home/brwnll/broken-promises/",
+    base: "/home/brwnll/broken-promises/test",
+    path: "/home/brwnll/broken-promises/test/test.js",
+    contents: new Buffer("/* @version 100.4.NEXT */"),
+  });
 
-	var stream = filever();
+  var stream = filever();
 
-	stream.on('data', function(newFile) {
-    t.ok(newFile, 'emits a file');
-		t.ok(newFile.path, 'file has a path');
-    t.ok(newFile.relative, 'file has relative path information');
-		t.ok(newFile.contents, 'file has contents');
+  stream.on("data", function (newFile) {
+    t.ok(newFile, "emits a file");
+    t.ok(newFile.path, "file has a path");
+    t.ok(newFile.relative, "file has relative path information");
+    t.ok(newFile.contents, "file has contents");
 
-    t.ok(newFile instanceof Vinyl, 'file is Vinyl');
-		t.ok(newFile.contents instanceof Buffer, 'file contents are a buffer');
+    t.ok(newFile instanceof Vinyl, "file is Vinyl");
+    t.ok(newFile.contents instanceof Buffer, "file contents are a buffer");
 
-    t.equals(newFile.relative, 'test-100.4.NEXT.js');
-	});
+    t.equals(newFile.relative, "test-100.4.NEXT.js");
+  });
 
-	stream.write(testFile1);
-	stream.end();
+  stream.write(testFile1);
+  stream.end();
 });
 
-test('should rename file with specified key', function(t) {
-	t.plan(1);
+test("should rename file with specified key", function (t) {
+  t.plan(1);
 
-	var testFile1 = new Vinyl({
-		cwd: "/home/brwnll/broken-promises/",
-		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test.js",
-		contents: new Buffer('/* @specialKey 100.4.NEXT */')
-	});
+  var testFile1 = new Vinyl({
+    cwd: "/home/brwnll/broken-promises/",
+    base: "/home/brwnll/broken-promises/test",
+    path: "/home/brwnll/broken-promises/test/test.js",
+    contents: new Buffer("/* @specialKey 100.4.NEXT */"),
+  });
 
-	var stream = filever({ key: 'specialKey' });
+  var stream = filever({ key: "specialKey" });
 
-	stream.on('data', function(newFile) {
+  stream.on("data", function (newFile) {
+    t.equals(newFile.relative, "test-100.4.NEXT.js");
+  });
 
-    t.equals(newFile.relative, 'test-100.4.NEXT.js');
-	});
-
-	stream.write(testFile1);
-	stream.end();
+  stream.write(testFile1);
+  stream.end();
 });
 
-test('should throw error when version missing', function(t) {
-	t.plan(1);
+test("should throw error when version missing", function (t) {
+  t.plan(1);
 
-	var testFile1 = new Vinyl({
-		cwd: "/home/brwnll/broken-promises/",
-		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test1.js",
-		contents: new Buffer('/* @specialKey 100.4.N */')
-	});
+  var testFile1 = new Vinyl({
+    cwd: "/home/brwnll/broken-promises/",
+    base: "/home/brwnll/broken-promises/test",
+    path: "/home/brwnll/broken-promises/test/test1.js",
+    contents: new Buffer("/* @specialKey 100.4.N */"),
+  });
 
-	var stream = filever();
+  var stream = filever();
 
-	stream.on('data', function(newFile) {	});
+  stream.on("data", function (newFile) {});
 
-  stream.on('error', function(err) {
-      t.equals(err.toString(), 'Error: Missing version key @version in test1.js');
-  })
+  stream.on("error", function (err) {
+    t.equals(err.toString(), "Error: Missing version key @version in test1.js");
+  });
 
-	stream.write(testFile1);
-	stream.end();
+  stream.write(testFile1);
+  stream.end();
 });
 
-test('should ignore error when silenced', function(t) {
-	t.plan(0);
+test("should ignore error when silenced", function (t) {
+  t.plan(0);
 
-	var testFile1 = new Vinyl({
-		cwd: "/home/brwnll/broken-promises/",
-		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test.js",
-		contents: new Buffer('/* @specialKey 100.WRONGKEY */')
-	});
+  var testFile1 = new Vinyl({
+    cwd: "/home/brwnll/broken-promises/",
+    base: "/home/brwnll/broken-promises/test",
+    path: "/home/brwnll/broken-promises/test/test.js",
+    contents: new Buffer("/* @specialKey 100.WRONGKEY */"),
+  });
 
-	var stream = filever({ silent: true });
+  var stream = filever({ silent: true });
 
-	stream.on('data', function(){ });
+  stream.on("data", function () {});
 
-  stream.on('error', function(err) { t.end(err) })
+  stream.on("error", function (err) {
+    t.end(err);
+  });
 
-  stream.on('end', function() { t.end(); })
+  stream.on("end", function () {
+    t.end();
+  });
 
-	stream.write(testFile1);
-	stream.end();
+  stream.write(testFile1);
+  stream.end();
 });
 
-test('should update sourcemaps', function(t) {
-	t.plan(2);
+test("should update sourcemaps", function (t) {
+  t.plan(2);
 
-	var testContent = '/* @version 0.0.1 */(function(first, second) {\n console.log(first + second);\n}(5, 10));\n';
+  var testContent =
+    "/* @version 0.0.1 */(function(first, second) {\n console.log(first + second);\n}(5, 10));\n";
 
-	var testFile = new Vinyl({
-		cwd: "/home/brwnll/broken-promises/",
-		base: "/home/brwnll/broken-promises/test",
-		path: "/home/brwnll/broken-promises/test/test.js",
-		contents: new Buffer(testContent)
-	});
+  var testFile = new Vinyl({
+    cwd: "/home/brwnll/broken-promises/",
+    base: "/home/brwnll/broken-promises/test",
+    path: "/home/brwnll/broken-promises/test/test.js",
+    contents: new Buffer(testContent),
+  });
 
-	var sm = sourcemaps.init();
+  var sm = sourcemaps.init();
 
-	var renamePipe = sm.pipe(filever());
+  var renamePipe = sm.pipe(filever());
 
-	renamePipe.on('data', function(newFile) {
-		t.ok(newFile.sourceMap, 'has a source map');
-		t.equals(newFile.sourceMap.file, 'test-0.0.1.js', 'renamed file in sourcefile link')
-	});
+  renamePipe.on("data", function (newFile) {
+    t.ok(newFile.sourceMap, "has a source map");
+    t.equals(
+      newFile.sourceMap.file,
+      "test-0.0.1.js",
+      "renamed file in sourcefile link"
+    );
+  });
 
-	sm.write(testFile);
-	sm.end();
-})
+  sm.write(testFile);
+  sm.end();
+});
